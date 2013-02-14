6a60f83f2b06c237858c7e202901c70b991b9b90
Florent Morin <morinflorent@gmail.com>
2013-02-14 18:51:14 +0100

    append multiroute to post contacts

--- server.rb
+++ server.rb
@@ -132,10 +132,14 @@ end
 post '/api/contacts/', '/api/contacts' do
   payload = request.params
     
-#  if payload.nil? ||  payload.empty?
-#    raw = request.env["rack.input"].read
-#    payload = JSON.parse raw
-#  end
+  if payload.nil? ||  payload.empty?
+    raw = request.env["rack.input"].read
+    payload = JSON.parse raw
+  end
+  
+  if payload[:id].present?
+    payload.delete :id
+  end
   
   c = Contact.new
   c.attributes = payload
@@ -150,10 +154,10 @@ end
 put '/api/contacts/:id' do |id|
   payload = request.params
     
-#  if payload.nil? ||  payload.empty?
-#    raw = request.env["rack.input"].read
-#    payload = JSON.parse raw
-#  end
+  if payload.nil? ||  payload.empty?
+    raw = request.env["rack.input"].read
+    payload = JSON.parse raw
+  end
   
   c = Contact.get(id)
   
